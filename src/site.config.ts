415369203f6e94f3f0fe167f87d1157f7f188044
Sergey Nikiforov <ser.nikiforov@gmail.com>
2025-08-18 02:14:25 +0300

    подключение комментариев

--- src/site.config.ts
+++ src/site.config.ts
@@ -98,10 +98,10 @@ const config: SiteConfig = {
   // Take the values from the generated script tag at https://giscus.app and fill them in here.
   // If you don't want to use Giscus, set this to undefined.
   giscus: {
-    repo: 'stelcodes/multiterm-astro',
-    repoId: 'R_kgDOPNnBig',
-    category: 'Giscus',
-    categoryId: 'DIC_kwDOPNnBis4CteOc',
+    repo: 'holyergo/holyergo.github.io',
+    repoId: 'R_kgDOPflYsg',
+    category: 'General',
+    categoryId: 'DIC_kwDOPflYss4CuSBB',
     reactionsEnabled: true, // Enable reactions on post itself
   },
 }
